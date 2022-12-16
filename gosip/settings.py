d3932a7046e7fc6596671df451e53c4346732fbe
Folarin Ogungbemi <Folarine1@gmail.com>
2022-12-16 06:09:51 +0000

    render templates index.html

--- gosip/settings.py
+++ gosip/settings.py
@@ -72,6 +72,7 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
             os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
         ],
         'APP_DIRS': True,
         'OPTIONS': {
