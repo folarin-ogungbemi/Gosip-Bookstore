56dcee43ee01b3466061efbb5ffe641249e77311
Folarin Ogungbemi <Folarine1@gmail.com>
2022-12-16 23:38:45 +0000

    Add about page and link urls

--- home/views.py
+++ home/views.py
@@ -4,3 +4,8 @@ from django.shortcuts import render
 def index(request):
     """ A view to render the home page """
     return render(request, 'home/index.html')
+
+
+def about(request):
+    """ A view to render the about page """
+    return render(request, 'home/about.html')
