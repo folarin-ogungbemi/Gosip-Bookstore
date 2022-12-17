fc6b9884469a6c856070c7dbc77be35a58514db4
Folarin Ogungbemi <Folarine1@gmail.com>
2022-12-17 02:09:01 +0000

    Add contact model

--- home/models.py
+++ home/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Contact(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone_number = models.IntegerField(null=True, blank=True)
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.email
