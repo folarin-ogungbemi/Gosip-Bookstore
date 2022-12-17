02e42179f0bd6045e7356ef34e3a0efc10ddaa3e
Folarin Ogungbemi <Folarine1@gmail.com>
2022-12-17 21:33:42 +0000

    Create and render book detail view in book detail template

--- books/urls.py
+++ books/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 app_name = 'books'
 urlpatterns = [
-    path('', views.BookViews.as_view(), name='books')
+    path('', views.BookViews.as_view(), name='books'),
+    path('<slug:slug>', views.BookDetailView.as_view(), name='book-details'),
 ]
--- books/views.py
+++ books/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from books .models import Books
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+
 
 
 class BookViews(ListView):
@@ -8,3 +10,9 @@ class BookViews(ListView):
     template_name = "books/books.html"
     context_object_name = "books"
     paginate_by = 8
+
+
+class BookDetailView(DetailView):
+    model = Books
+    template_name = 'books/book-details.html'
+    context_object_name = 'book'
