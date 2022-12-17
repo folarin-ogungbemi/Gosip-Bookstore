dedcaa99cd430576e628dc0c26b0023d5ed9afe4
Folarin Ogungbemi <Folarine1@gmail.com>
2022-12-17 05:38:57 +0000

    Fix 404 bug: project urls allauth path

--- gosip/urls.py
+++ gosip/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('allauth.urls')),
+    path('account/', include('allauth.urls')),
     path('summernote/', include('django_summernote.urls')),
     path('', include('home.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
