c1925d9e70690c7279280da694ee403c359bb38e
Folarin Ogungbemi <Folarine1@gmail.com>
2022-12-17 16:00:52 +0000

    Create models for books and register

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from books.models import Author, Special, Genre, Books
 
 # Register your models here.
+admin.site.register(Author)
+admin.site.register(Special)
+admin.site.register(Genre)
+admin.site.register(Books)
--- books/models.py
+++ books/models.py
@@ -1,3 +1,94 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+from django.core.validators import MinValueValidator
 
-# Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(
+        max_length=100,
+        null=False,
+        blank=False)
+    book_title = models.CharField(
+        max_length=100,
+        null=False,
+        blank=False)
+    about = models.TextField()
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+
+class Special(models.Model):
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+
+RATING = [
+    (1, 'very bad'),
+    (2, 'bad'),
+    (3, 'okay'),
+    (4, 'good'),
+    (5, 'excellent'),
+]
+
+
+class Books(models.Model):
+    title = models.CharField(max_length=250, unique=True)
+    slug = models.SlugField(max_length=260, unique=True)
+    author = models.ForeignKey(
+        Author,
+        on_delete=models.CASCADE,
+        related_name='book')
+    genre = models.ForeignKey(
+        Genre,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='book_category')
+    special = models.ManyToManyField(
+        Special,
+        blank=True)
+    publication_year = models.IntegerField()
+    pages = models.IntegerField()
+    language = models.CharField(max_length=100)
+    isbn = models.IntegerField(unique=True)
+    dimension = models.CharField(max_length=100)
+    price = models.DecimalField(
+        max_digits=6,
+        decimal_places=2,
+        validators=[MinValueValidator(0)])
+    rating = models.PositiveIntegerField(
+        choices=RATING,
+        null=True,
+        blank=True)
+    description = models.TextField(
+        null=False,
+        blank=False
+    )
+    image = CloudinaryField('image')
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return f"{self.title} by {self.author}"
