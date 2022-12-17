02fade43f015d5a579f4c3c299bf8abd85f006ad
Folarin Ogungbemi <Folarine1@gmail.com>
2022-12-17 00:26:01 +0000

    Render team page and structure

--- home/views.py
+++ home/views.py
@@ -9,3 +9,9 @@ def index(request):
 def about(request):
     """ A view to render the about page """
     return render(request, 'home/about.html')
+
+
+def team(request):
+    """ A view to render the team page """
+    return render(request, 'home/team.html')
+
