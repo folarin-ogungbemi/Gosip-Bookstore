7820929b01e75e33bc37e471c590cc78e2573e9b
Folarin Ogungbemi <Folarine1@gmail.com>
2022-12-18 17:35:31 +0000

    Create Order model and register in admin

--- cart/admin.py
+++ cart/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Order
+
 
 # Register your models here.
+@admin.register(Order)
+class OrdersAdmin(admin.ModelAdmin):
+    list_display = (
+        'order_id', 'item', 'customer',
+        'quantity', 'order_date',
+        'concluded')
+    search_fields = ('item', 'order_id', 'order_date', 'concluded')
+    list_filter = ('item', 'order_id', 'concluded')
--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+import uuid
+from books.models import Books
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Order(models.Model):
+    order_id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False)
+    item = models.ForeignKey(Books, on_delete=models.CASCADE)
+    customer = models.ForeignKey(User, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    concluded = models.BooleanField(default=False)
+    order_date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-order_id']
+
+    def __str__(self):
+        return str(self.order_id)
