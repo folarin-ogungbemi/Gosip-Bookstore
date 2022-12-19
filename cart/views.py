d79a27ed6ca01cf0706acfc86434d8bf6044e8f5
Folarin Ogungbemi <Folarine1@gmail.com>
2022-12-19 18:30:46 +0000

    Add order item to cart

--- cart/views.py
+++ cart/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from cart.models import Order
 from django.views.generic import ListView
+from django.shortcuts import get_object_or_404
 
 
 class OrderView(ListView):
@@ -8,3 +9,18 @@ class OrderView(ListView):
     model = Order
     template_name = 'cart/shopping_cart.html'
     context_object_name = 'order_items'
+
+
+def order_item(request, slug):
+    """ Add item in quantity to cart"""
+
+    qty = int(request.POST.get('quantity'))
+    cart = request.session.get('cart', {})
+
+    if slug in list(cart.keys()):
+        cart[slug] += qty
+    else:
+        cart[slug] = qty
+    request.session['cart'] = cart
+    print(request.session['cart'])
+    return redirect('shopping_cart')
