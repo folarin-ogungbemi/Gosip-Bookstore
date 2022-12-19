20ee731e816c999088cb1a5b9adc5147253db030
Folarin Ogungbemi <Folarine1@gmail.com>
2022-12-19 18:46:08 +0000

    Update context processor

--- cart/contexts.py
+++ cart/contexts.py
@@ -1,11 +1,24 @@
 from django.conf import settings
 from decimal import Decimal
+from django.shortcuts import get_object_or_404
+from books.models import Books
 
 
 def shopping_cart(request):
     shopping_cart = []
     total = 0
     book_count = 0
+    cart = request.session.get('cart', {})
+
+    for slug, qty in cart.items():
+        book = get_object_or_404(Books, slug=slug)
+        total += qty * book.price
+        book_count += qty
+        shopping_cart.append({
+            'slug': slug,
+            'book': book,
+            'qty': qty,
+        })
 
     tax_deduction = total * Decimal(settings.TAX/100)
 
