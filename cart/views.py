1df5b632527a2ef0ecd7c17b04490ad0d768ced0
Folarin Ogungbemi <Folarine1@gmail.com>
2022-12-21 11:30:53 +0000

    Fix ValueError: when no data is entered in input

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.urls import reverse
 from django.views.generic import ListView
 from django.shortcuts import get_object_or_404
 
@@ -11,12 +12,16 @@ def order_view(request):
 def order_item(request, slug):
     """ Add item in quantity to cart"""
 
-    qty = int(request.POST.get('quantity'))
-    cart = request.session.get('cart', {})
+    try:
+        qty = int(request.POST.get('quantity'))
+        cart = request.session.get('cart', {})
 
-    if slug in list(cart.keys()):
-        cart[slug] += qty
-    else:
-        cart[slug] = qty
-    request.session['cart'] = cart
-    return redirect('shopping_cart')
+        if slug in list(cart.keys()):
+            cart[slug] += qty
+        else:
+            cart[slug] = qty
+        request.session['cart'] = cart
+        return redirect('shopping_cart')
+    except ValueError:
+        # return a message to user on invaild input
+        return redirect(reverse('books:book-details', args=[slug]))
