e62d3d502a85a49d4c7ec982fc716deafe257a0e
Folarin Ogungbemi <Folarine1@gmail.com>
2022-12-22 00:09:49 +0000

    Add Toast messages on different levels and style

--- cart/views.py
+++ cart/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.views.generic import ListView
 from django.shortcuts import get_object_or_404
+from django.contrib import messages
+from books.models import Books
 
 
 def order_view(request):
@@ -11,17 +13,27 @@ def order_view(request):
 
 def order_item(request, slug):
     """ Add item in quantity to cart"""
-
+    book = Books.objects.get(slug=slug)
     try:
         qty = int(request.POST.get('quantity'))
         cart = request.session.get('cart', {})
 
         if slug in list(cart.keys()):
             cart[slug] += qty
+            messages.success(
+                request,
+                f'"{book.title}" quantity updated to ({cart[slug]})')
         else:
             cart[slug] = qty
+            messages.success(
+                request,
+                f'"{book.title}" added to your cart')
+
         request.session['cart'] = cart
         return redirect('shopping_cart')
     except ValueError:
         # return a message to user on invaild input
+        messages.info(
+            request,
+            f'Please enter a valid quantity for "{book.title}"')
         return redirect(reverse('books:book-details', args=[slug]))
--- gosip/settings.py
+++ gosip/settings.py
@@ -96,6 +96,9 @@ TEMPLATES = [
     },
 ]
 
+# required due to the use of gitpod
+MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
+
 AUTHENTICATION_BACKENDS = [
     # Needed to login by username in Django admin, regardless of `allauth`
     'django.contrib.auth.backends.ModelBackend',
