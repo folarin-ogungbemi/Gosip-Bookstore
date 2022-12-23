bd581809b7b0b691250e2b5a40ebcc3f5154e478
Folarin Ogungbemi <Folarine1@gmail.com>
2022-12-23 23:25:59 +0000

    Save checkout form in db

--- checkout/views.py
+++ checkout/views.py
@@ -14,23 +14,45 @@ def checkout_view(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
 
-    cart = request.session.get('cart', {})
+    if request.method == 'POST':
+        cart = request.session.get('cart', {})
 
-    # Display Error alert when Cart is empty
-    if not cart:
-        messages.error(request, "Your Cart is Empty at the moment")
-        return redirect(reverse('books:books'))
+        customer_data = {
+            'first_name': request.POST['first_name'],
+            'last_name': request.POST['last_name'],
+            'email': request.POST['email'],
+            'phone_number': request.POST['phone_number'],
+            'house_number': request.POST['house_number'],
+            'street': request.POST['street'],
+            'postal_address': request.POST['postal_address'],
+            'city': request.POST['city'],
+            'country': request.POST['country'],
+        }
 
-    cart_items = shopping_cart(request)
-    total = cart_items['grand_total']
-    stripe_total = round(total * 100)
-    stripe.api_key = stripe_secret_key
-    intent = stripe.PaymentIntent.create(
-        amount=stripe_total,
-        currency=settings.STRIPE_CURRENCY,
-    )
+        form = OrderForm(customer_data)
+        if form.is_valid():
+            order = form.save()
+            print("-----------------------")
+            print(order)
+            print("-----------------------")
 
-    form = OrderForm()
+    else:
+        # Display Error alert when Cart is empty
+        cart = request.session.get('cart', {})
+        if not cart:
+            messages.error(request, "Your Cart is Empty at the moment")
+            return redirect(reverse('books:books'))
+
+        cart_items = shopping_cart(request)
+        total = cart_items['grand_total']
+        stripe_total = round(total * 100)
+        stripe.api_key = stripe_secret_key
+        intent = stripe.PaymentIntent.create(
+            amount=stripe_total,
+            currency=settings.STRIPE_CURRENCY,
+        )
+
+        form = OrderForm()
 
     context = {
         'form': form,
