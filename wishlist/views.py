fcb8d37db08169f8f268c901ca83fe083d54bc69
Folarin Ogungbemi <Folarine1@gmail.com>
2022-12-27 11:57:53 +0000

    render wish view

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,1 +1,9 @@
 from django.shortcuts import render
+
+
+def wishlist_view(request):
+    """ function renders wishlist view """
+    # context = {
+    #     'wishes': wishes,
+    # }
+    return render(request, 'wishlist/wish.html')
