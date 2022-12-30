84a79a8dea4e31669ebdd6aaede02282b99d256f
Folarin Ogungbemi <Folarine1@gmail.com>
2022-12-30 21:53:01 +0000

    Create userProfile models and register admin

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import UserProfile
+
 
 # Register your models here.
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = [
+        'user', 'user_phone_number',
+        'user_address_line_1', 'user_address_line_2',
+        'user_city', 'user_zip', 'user_state', 'user_country'
+    ]
+    search_fields = ('user', 'user_city', 'user_country')
+    list_filter = ('user', 'user_city', 'user_country')
--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+from django_countries.fields import CountryField
+
+
+class UserProfile(models.Model):
+    """
+    A user profile model for maintaining user
+    delivery information and order history
+    """
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE)
+    user_phone_number = models.CharField(
+        max_length=17, null=True, blank=True)
+    user_address_line_1 = models.CharField(
+        max_length=254, null=True, blank=True)
+    user_address_line_2 = models.CharField(
+        max_length=254, null=True, blank=True)
+    user_zip = models.CharField(
+        max_length=10, null=True, blank=True)
+    user_city = models.CharField(
+        max_length=100, null=True, blank=True)
+    user_state = models.CharField(
+        max_length=100,  null=True, blank=True)
+    user_country = CountryField(
+        blank_label='Country', null=True, blank=True)
+
+    def __str__(self):
+        return self.user.username
