1f957d0f7b72d9ed7a4a4ae1b7e67df57fe40723
Folarin Ogungbemi <Folarine1@gmail.com>
2023-01-01 20:57:26 +0000

    complete post method on profile form

--- profiles/views.py
+++ profiles/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from profiles.models import UserProfile
 from profiles.forms import UserProfileForm
 from django.shortcuts import get_object_or_404
+from django.contrib import messages
 
 
 def profile_view(request):
@@ -10,9 +11,17 @@ def profile_view(request):
     form = UserProfileForm(instance=profile)
     orders = profile.orders.all()
 
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(
+                request, 'Your profile has been updated successfully')
+
     context = {
             'profile': profile,
             'form': form,
-            'orders': orders
+            'orders': orders,
+            'on_profile_page': True,
         }
     return render(request, "profiles/user_profile.html", context)
