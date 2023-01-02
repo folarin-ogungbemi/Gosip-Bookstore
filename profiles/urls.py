384aefed77836ea0a2ac0dfc7ec837626aba109e
Folarin Ogungbemi <Folarine1@gmail.com>
2023-01-02 06:36:31 +0000

    Create function to view completed transaction

--- profiles/urls.py
+++ profiles/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.profile_view, name='user_profile'),
+    path('order_history/<order_id>', views.order_history, name='order_history'),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
 from profiles.models import UserProfile
 from profiles.forms import UserProfileForm
+from checkout.models import Order
 from django.shortcuts import get_object_or_404
 from django.contrib import messages
 
 
 def profile_view(request):
-    """Render the user's profile"""
+    """
+    Render the user's profile view, order history and
+    allow user to update their data.
+    """
     profile = get_object_or_404(UserProfile, user=request.user)
     form = UserProfileForm(instance=profile)
     orders = profile.orders.all()
@@ -25,3 +29,14 @@ def profile_view(request):
             'on_profile_page': True,
         }
     return render(request, "profiles/user_profile.html", context)
+
+
+def order_history(request, order_id):
+    order = get_object_or_404(Order, order_id=order_id)
+
+    context = {
+        'order': order,
+        'from_profile': True,
+    }
+
+    return render(request, 'checkout/transact_success.html', context)
