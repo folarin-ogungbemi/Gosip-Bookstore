08564ee5b7dab312d76501f0a60909401f4476b2
Folarin Ogungbemi <Folarine1@gmail.com>
2023-01-04 09:41:31 +0000

    Adjust to meet line length requirement

--- profiles/urls.py
+++ profiles/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.profile_view, name='user_profile'),
-    path('order_history/<order_id>', views.order_history, name='order_history'),
+    path(
+        'order_history/<order_id>',
+        views.order_history, name='order_history'),
 ]
