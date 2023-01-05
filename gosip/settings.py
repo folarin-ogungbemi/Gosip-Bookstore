524636deeba2c4f042610eefd9cec70b2ae942fa
Folarin Ogungbemi <Folarine1@gmail.com>
2023-01-05 18:17:30 +0000

    correct custom domain aws

--- gosip/settings.py
+++ gosip/settings.py
@@ -226,7 +226,7 @@ if 'USE_AWS' in os.environ:
     AWS_S3_REGION_NAME = 'us-east-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-    AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
+    AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3-website-us-east-1.amazonaws.com"
 
     # Static and Media files
     STATICFILES_STORAGE = 'custom_storages.StaticStorage'
