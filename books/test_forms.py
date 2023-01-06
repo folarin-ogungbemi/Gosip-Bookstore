f0c7fde819c3f644c08c9fcfa20321cd86dc63c6
Folarin Ogungbemi <Folarine1@gmail.com>
2023-01-06 10:52:34 +0000

    Add test files

---
+++ books/test_forms.py
@@ -0,0 +1,49 @@
+from django.test import TestCase
+from .forms import AuthorForm, BookForm
+
+
+# Create your tests here.
+class TestAuthorForm(TestCase):
+
+    def test_author_name_is_required(self):
+        form = AuthorForm({'name': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('name', form.errors.keys())
+        self.assertEqual(form.errors['name'][0], 'This field is required.')
+
+    def test_author_book_title_is_required(self):
+        form = AuthorForm({'book_title': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('book_title', form.errors.keys())
+        self.assertEqual(form.errors['book_title'][0], 'This field is required.')
+
+    def test_author_about_is_required(self):
+        form = AuthorForm({'about': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('about', form.errors.keys())
+        self.assertEqual(form.errors['about'][0], 'This field is required.')
+
+
+class TestBookForm(TestCase):
+
+    def test_title_is_required(self):
+        form = BookForm({'title': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('title', form.errors.keys())
+        self.assertEqual(form.errors['title'][0], 'This field is required.')
+
+    def test_slug_is_required(self):
+        form = BookForm({'slug': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('slug', form.errors.keys())
+        self.assertEqual(form.errors['slug'][0], 'This field is required.')
+
+    def test_author_is_required(self):
+        form = BookForm({'author': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('author', form.errors.keys())
+        self.assertEqual(form.errors['author'][0], 'This field is required.')
+
+    def test_genre_is_not_required(self):
+        form = BookForm({'genre': ''})
+        self.assertFalse(form.is_valid())
