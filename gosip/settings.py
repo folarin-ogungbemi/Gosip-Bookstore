127f88c39b5d1c014beb20dc9dfaf1a3d4c7f718
Folarin Ogungbemi <Folarine1@gmail.com>
2023-02-14 23:56:29 +0000

    uninstall cloudinary storage

--- gosip/settings.py
+++ gosip/settings.py
@@ -10,13 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-from pathlib import Path
-from django.contrib.messages import constants as messages
-
 import os
+from pathlib import Path
 import dj_database_url
-
-if os.path.isfile('env.py'):
+from django.contrib.messages import constants as messages
+if os.path.isfile("env.py"):
     import env
 development = os.environ.get('DEVELOPMENT', False)
 
@@ -48,10 +46,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     # django messages
     'django.contrib.messages',
-    # cloudinary
-    'cloudinary_storage',
-    'django.contrib.staticfiles',
-    'cloudinary',
     # allauth
     'django.contrib.sites',
     'allauth',
