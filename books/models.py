a0dac3733ec2b0e51feae4f303b55646b2e107b7
Folarin Ogungbemi <Folarine1@gmail.com>
2023-02-15 00:37:50 +0000

    Add image fields to books model

--- books/models.py
+++ books/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from cloudinary.models import CloudinaryField
 from django.core.validators import MinValueValidator
 
 
@@ -84,7 +83,8 @@ class Books(models.Model):
         null=False,
         blank=False
     )
-    image = CloudinaryField('image')
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
     created_on = models.DateTimeField(auto_now_add=True)
 
     class Meta:
