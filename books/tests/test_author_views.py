f1cde49fe954c5a6aef1d536e45e3ab2406785b4
Folarin Ogungbemi <Folarine1@gmail.com>
2023-02-24 10:51:09 +0000

    Test add author views

---
+++ books/tests/test_author_views.py
@@ -0,0 +1,68 @@
+from django.test import TestCase, Client
+from django.urls import reverse
+from books.models import Author, Genre, Special, Books
+# authentication
+from django.contrib.auth.models import User
+
+
+class TestAuthorViews(TestCase):
+    """Test Author views"""
+
+    def setUp(self):
+        self.client = Client()
+        # create a admin user
+        self.admin_user = User.objects.create_superuser(
+            username='admin',
+            password='password123',
+            email='admin@example.com'
+        )
+        # create a non-admin user
+        user = User.objects.create_user(
+            username='user',
+            password='password',
+            email='user@example.com'
+        )
+        self.author_data = {
+            'name': 'Test Author',
+            'book_title': 'test book title',
+            'about': 'testing'}
+        self.books_url = reverse('books:books')
+        self.add_author_url = reverse('books:add_author')
+        self.author = Author.objects.create(
+            name='test', book_title='book title', about='tester')
+
+    def test_add_author_successfully(self):
+        # login as an admin
+        self.client.login(username='admin', password='password123')
+        # make a post request
+        response = self.client.post(self.add_author_url, self.author_data)
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse('books:add_book'))
+        # count number of books
+        self.assertTrue(Author.objects.all().count(), 1)
+        # check existence of book is true
+        self.assertTrue(Author.objects.filter(name='Test Author').exists())
+
+    def test_add_author_unauthorized(self):
+        # login as a user
+        self.client.login(username='user', password='password')
+        # make a post request
+        response = self.client.post(self.add_author_url, self.author_data)
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse('home'))
+        # check existence of book is false
+        self.assertFalse(Author.objects.filter(name='Test Author').exists())
+
+    def test_add_author_invalid_form(self):
+        # login as an admin
+        self.client.login(username='admin', password='password123')
+        # required 'name' field not filled
+        self.author_data['name'] = ""
+        # make a POST request to the URL
+        response = self.client.post(self.add_author_url, self.author_data)
+        self.assertEqual(
+            response.request.get("PATH_INFO"), self.add_author_url)
+        self.assertFormError(
+            response, 'form', 'name', 'This field is required.')
+        # count number of books
+        self.assertEqual(Books.objects.count(), 0)
