81321f5b13368029ef445d89d8ef02716a1621df
Folarin Ogungbemi <Folarine1@gmail.com>
2023-02-25 10:53:48 +0000

    Fix: home app lint issues

--- home/tests/test_views.py
+++ home/tests/test_views.py
@@ -1,7 +1,6 @@
-from django.test import TestCase, Client
+from django.test import TestCase
 from django.urls import reverse
 from home.models import Contact
-import json
 
 
 class TestViews(TestCase):
